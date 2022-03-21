8c8e9eb7625ab6eb8cddb1497532b04c3d1ffffc
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-21 21:39:07 +0530

    Use activity instead of fragment

--- app/src/main/java/com/everest/moviedb/ui/DetailsActivity.kt
+++ app/src/main/java/com/everest/moviedb/ui/DetailsActivity.kt
@@ -1,4 +1,31 @@
 package com.everest.moviedb.ui
 
-class DetailsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.everest.moviedb.databinding.ActivityDetailsBinding
+import com.everest.moviedb.models.Movie
+import com.everest.moviedb.network.RetrofitClient
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        displayMovieDetails()
+
+    }
+
+    private fun displayMovieDetails() {
+        val movie: Movie? = intent.getParcelableExtra<Movie>("MOVIE_DETAILS")
+        binding.movieTitleTV.text = movie?.title
+        binding.movieDescriptionTV.text = movie?.overview
+        Glide.with(this)
+            .load(RetrofitClient.image_base_url + movie?.poster_path)
+            .into(binding.movieThumbnailTV)
+    }
 }
