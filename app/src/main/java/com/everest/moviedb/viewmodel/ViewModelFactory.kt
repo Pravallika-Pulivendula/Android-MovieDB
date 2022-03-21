c885d18a7886c29a7406b77749304d70748530e4
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-21 21:42:51 +0530

    Add view model factory

--- app/src/main/java/com/everest/moviedb/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/everest/moviedb/viewmodel/ViewModelFactory.kt
@@ -1,4 +1,16 @@
 package com.everest.moviedb.viewmodel
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.everest.moviedb.network.RetrofitRepository
+
+class ViewModelFactory constructor(private val retrofitRepository: RetrofitRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(RepositoryViewModel::class.java)) {
+            RepositoryViewModel(this.retrofitRepository) as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
 }
