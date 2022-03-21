6eec02621c207a8a2cbf7912d7cb83907ca5973f
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-21 12:54:36 +0530

    Add main activity

--- app/src/main/java/com/everest/moviedb/ui/MainActivity.kt
+++ app/src/main/java/com/everest/moviedb/ui/MainActivity.kt
@@ -1,4 +1,28 @@
 package com.everest.moviedb.ui
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.everest.moviedb.R
+import com.everest.moviedb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var homeFragment: HomeFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeFragment = HomeFragment()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment()
+    }
+
+    private fun loadFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_one, HomeFragment())
+            addToBackStack(null)
+            commit()
+        }
+    }
 }
