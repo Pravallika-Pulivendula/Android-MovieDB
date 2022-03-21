42d046b735286776f6a906cebc13741ca5200741
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-21 21:38:02 +0530

    Add repository

--- app/src/main/java/com/everest/moviedb/network/RetrofitRepository.kt
+++ app/src/main/java/com/everest/moviedb/network/RetrofitRepository.kt
@@ -1,4 +1,10 @@
 package com.everest.moviedb.network
 
-class RetrofitRepository {
+class RetrofitRepository(private val retrofitService: RetrofitService) {
+    fun getPopularMovies() = retrofitService.getPopularMovies(RetrofitClient.api_key)
+
+    fun getLatestMovies() = retrofitService.getLatestMovies(RetrofitClient.api_key)
+
+    fun searchMovieByName(movieName: String) =
+        retrofitService.getMovieByName(RetrofitClient.api_key, movieName)
 }
