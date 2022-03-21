14c0390c6a018b4e86d602cbc30648b650d78cc9
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-21 21:38:28 +0530

    Rename class name

--- app/src/main/java/com/everest/moviedb/network/RetrofitService.kt
+++ app/src/main/java/com/everest/moviedb/network/RetrofitService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface RetrofitApi {
+interface RetrofitService {
     @GET("movie/popular")
     fun getPopularMovies(@Query("api_key") api_key: String): Call<MovieResponse>
 
