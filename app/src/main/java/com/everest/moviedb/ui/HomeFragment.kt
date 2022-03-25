6dedd4cc7673b930c9f9a58b5c457d073bff9855
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-25 22:24:20 +0530

    override onViewCreate method

--- app/src/main/java/com/everest/moviedb/ui/HomeFragment.kt
+++ app/src/main/java/com/everest/moviedb/ui/HomeFragment.kt
@@ -10,7 +10,6 @@ import com.everest.moviedb.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,15 +17,18 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val viewPager = binding.viewPager
-        viewPager.adapter = PageAdapter(childFragmentManager)
-
-        val tabLayout = binding.tabLayout
-        tabLayout.setupWithViewPager(viewPager)
-
         setHasOptionsMenu(true)
 
-        return binding.root
+        return _binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewPager = _binding!!.viewPager
+        viewPager.adapter = PageAdapter(childFragmentManager)
+
+        val tabLayout = _binding!!.tabLayout
+        tabLayout.setupWithViewPager(viewPager)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
