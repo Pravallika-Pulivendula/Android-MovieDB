468febacba565a541e7294aca6c07c43c212f988
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-30 12:13:41 +0530

    Use string array

--- app/src/main/java/com/everest/moviedb/adapters/PageAdapter.kt
+++ app/src/main/java/com/everest/moviedb/adapters/PageAdapter.kt
@@ -1,21 +1,25 @@
 package com.everest.moviedb.adapters
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.everest.moviedb.R
 import com.everest.moviedb.ui.LatestMovieFragment
 import com.everest.moviedb.ui.PopularMovieFragment
 
-class PageAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+class PageAdapter(fragmentManager: FragmentManager, context: Context) :
+    FragmentStatePagerAdapter(fragmentManager) {
+
+    var pageTitles: Array<String> = context.resources.getStringArray(R.array.tab_titles)
+
     override fun getCount(): Int {
         return 2;
     }
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
-            0 -> {
-                PopularMovieFragment()
-            }
             1 -> {
                 LatestMovieFragment()
             }
@@ -28,10 +32,10 @@ class PageAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(
     override fun getPageTitle(position: Int): CharSequence? {
         when (position) {
             0 -> {
-                return "Popular"
+                return pageTitles[position]
             }
             1 -> {
-                return "Current Year"
+                return pageTitles[position]
             }
         }
         return super.getPageTitle(position)
